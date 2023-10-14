56f8803f77240d53e1bd12765d0b0c547b31a85e
ConstantineJin <ConstantineJin@outlook.com>
2023-10-14 10:56:40 +0800

    Add 136.Single_Number

---
+++ 136.Single_Number/136.Single_Number.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// 遍历，使用哈希表与空结构体实现set
+// type EST struct {
+// }
+
+// func singleNumber(nums []int) int {
+// 	set := make(map[int]EST)
+// 	for _, v := range nums {
+// 		if _, ok := set[v]; ok {
+// 			delete(set, v)
+// 		} else {
+// 			set[v] = EST{}
+// 		}
+// 	}
+// 	for k, _ := range set {
+// 		return k
+// 	}
+// 	return 0
+// }
+
+// 位运算，使用异或
+func singleNumber(nums []int) int {
+	single := 0
+	for _, num := range nums {
+		single ^= num
+	}
+	return single
+}
+
+func main() {
+	nums := []int{2, 2, 1}
+	fmt.Println(singleNumber(nums))
+}
