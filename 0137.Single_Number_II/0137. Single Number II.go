7a3707eb55f99334019e3c1aa77a645f3602783d
ConstantineJin <ConstantineJin@outlook.com>
2023-10-15 20:35:02 +0800

    add solution for 0137

---
+++ 0137.Single_Number_II/0137. Single Number II.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func singleNumber(nums []int) int {
+	ans := int32(0)
+	for i := 0; i < 32; i++ {
+		total := int32(0)
+		for _, num := range nums {
+			total += int32(num) >> i & 1
+		}
+		if total%3 > 0 {
+			ans |= 1 << i
+		}
+	}
+	return int(ans)
+}
+
+func main() {
+	nums := []int{2, 2, 3, 2}
+	fmt.Println(singleNumber(nums))
+}
