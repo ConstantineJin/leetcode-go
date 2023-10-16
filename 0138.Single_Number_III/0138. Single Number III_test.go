66aeec7fee99c6641af9fcc956790169c885c935
ConstantineJin <ConstantineJin@outlook.com>
2023-10-16 20:11:24 +0800

    Add test for 0138

---
+++ 0138.Single_Number_III/0138. Single Number III_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Example 1",
+			args: args{nums: []int{1, 2, 1, 3, 2, 5}},
+			want: []int{5, 3},
+		},
+		{
+			name: "Example 2",
+			args: args{nums: []int{-1, 0}},
+			want: []int{0, -1},
+		},
+		{
+			name: "Example 3",
+			args: args{nums: []int{0, 1}},
+			want: []int{0, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
