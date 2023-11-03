ed25352edce54b99cd470e78158b761eb5a7d88b
ConstantineJin <jzy666jzy@gmail.com>
2023-11-03 12:05:35 +0800

    Add 0117 tests

--- 0117.Populating_Next_Right_Pointers_in_Each_Node_II/0117. Populating Next Right Pointers in Each Node II_test.go
+++ 0117.Populating_Next_Right_Pointers_in_Each_Node_II/0117. Populating Next Right Pointers in Each Node II_test.go
@@ -5,6 +5,26 @@ import (
 	"testing"
 )
 
+func example1() *Node {
+	node7 := Node{Val: 7, Left: nil, Right: nil, Next: nil}
+	node5 := Node{Val: 5, Left: nil, Right: nil, Next: nil}
+	node4 := Node{Val: 4, Left: nil, Right: nil, Next: nil}
+	node3 := Node{Val: 3, Left: nil, Right: &node7, Next: nil}
+	node2 := Node{Val: 2, Left: &node4, Right: &node5, Next: nil}
+	node1 := Node{Val: 1, Left: &node2, Right: &node3, Next: nil}
+	return &node1
+}
+
+func want1() *Node {
+	node7 := Node{Val: 7, Left: nil, Right: nil, Next: nil}
+	node5 := Node{Val: 5, Left: nil, Right: nil, Next: &node7}
+	node4 := Node{Val: 4, Left: nil, Right: nil, Next: &node5}
+	node3 := Node{Val: 3, Left: nil, Right: &node7, Next: nil}
+	node2 := Node{Val: 2, Left: &node4, Right: &node5, Next: &node3}
+	node1 := Node{Val: 1, Left: &node2, Right: &node3, Next: nil}
+	return &node1
+}
+
 func Test_connect(t *testing.T) {
 	type args struct {
 		root *Node
@@ -14,42 +34,16 @@ func Test_connect(t *testing.T) {
 		args args
 		want *Node
 	}{
-		// TODO: Add test cases.
-		//{
-		//	name: "Example1",
-		//	args: args{root: &Node{
-		//		Val:   1,
-		//		Left:  &Node{
-		//			Val:   2,
-		//			Left:  &Node{
-		//				Val:   4,
-		//				Left:  nil,
-		//				Right: nil,
-		//				Next:  nil,
-		//			},
-		//			Right: &Node{
-		//				Val:   5,
-		//				Left:  nil,
-		//				Right: nil,
-		//				Next:  nil,
-		//			},
-		//			Next:  nil,
-		//		},
-		//		Right: &Node{
-		//			Val:   3,
-		//			Left:  nil,
-		//			Right: &Node{
-		//				Val:   7,
-		//				Left:  nil,
-		//				Right: nil,
-		//				Next:  nil,
-		//			},
-		//			Next:  nil,
-		//		},
-		//		Next:  nil,
-		//	}},
-		//	want:
-		//},
+		{
+			name: "Example1",
+			args: args{example1()},
+			want: want1(),
+		},
+		{
+			name: "Example2",
+			args: args{root: nil},
+			want: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
