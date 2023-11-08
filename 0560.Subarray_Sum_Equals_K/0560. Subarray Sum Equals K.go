a83637b56334ea1e183dd73e2d08bd326d8684ad
ConstantineJin <ConstantineJin@outlook.com>
2023-11-08 12:24:14 +0800

    Modify 0560 solutions and tests

--- 0560.Subarray_Sum_Equals_K/0560. Subarray Sum Equals K.go
+++ 0560.Subarray_Sum_Equals_K/0560. Subarray Sum Equals K.go
@@ -4,7 +4,7 @@ func subarraySum(nums []int, k int) (res int) {
 	pre, mp := 0, map[int]int{0: 1} // pre为前缀和，mp的key为和，value为出现次数
 	for i := 0; i < len(nums); i++ {
 		pre += nums[i]              // 更新前缀和
-		if _, ok := mp[pre-k]; ok { // mp中已有键pre-k
+		if _, ok := mp[pre-k]; ok { // mp中已有键pre-k，亦即从该处到当前所遍历至构成的子数字之和为k
 			res += mp[pre-k] // 结果添加对应的出现次数
 		}
 		mp[pre]++ // 更新mp
