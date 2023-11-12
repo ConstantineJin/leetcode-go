8ab1a203a63f02b0debd3c0ad4c72146f8e028a7
ConstantineJin <jzy666jzy@gmail.com>
2023-11-12 21:30:09 +0800

    Add 0056 solutions and tests

---
+++ 0056.Merge_Intervals/0056. Merge Intervals_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		intervals [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "Example1",
+			args: args{[][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}},
+			want: [][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			name: "Example2",
+			args: args{[][]int{{1, 4}, {4, 5}}},
+			want: [][]int{{1, 5}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := merge(tt.args.intervals); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("merge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
