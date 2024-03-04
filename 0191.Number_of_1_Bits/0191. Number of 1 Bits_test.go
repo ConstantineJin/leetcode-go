3d0c7d279222f44dceda4f2c0e3364e495229b0a
ConstantineJin <ConstantineJin@outlook.com>
2024-03-04 16:18:12 +0800

    Add 0190,0191 solutions and tests

---
+++ 0191.Number_of_1_Bits/0191. Number of 1 Bits_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Example1",
+			args: args{num: 11},
+			want: 3,
+		},
+		{
+			name: "Example2",
+			args: args{num: 128},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
