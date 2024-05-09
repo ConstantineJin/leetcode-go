aeb7206911cb7b6439b420510a401a931bf722b2
constantinejin <ConstantineJin@outlook.com>
2024-05-09 09:32:18 +0800

    Add 2925 solutions and tests

--- 2925.Maximum_Score_After_Applying_Operations_on_a_Tree/2925. Maximum Score After Applying Operations on a Tree.go
+++ 2925.Maximum_Score_After_Applying_Operations_on_a_Tree/2925. Maximum Score After Applying Operations on a Tree.go
@@ -4,13 +4,10 @@ func maximumScoreAfterOperations(edges [][]int, values []int) int64 {
 	g := make([][]int, len(values))
 	g[0] = append(g[0], -1) // 避免误把根节点当作叶子
 	for _, e := range edges {
-		x, y := e[0], e[1]
-		g[x] = append(g[x], y)
-		g[y] = append(g[y], x)
+		g[e[0]] = append(g[e[0]], e[1])
+		g[e[1]] = append(g[e[1]], e[0])
 	}
-
-	// 先把所有分数加入答案
-	ans := 0
+	var ans int
 	for _, v := range values {
 		ans += v
 	}
@@ -20,7 +17,7 @@ func maximumScoreAfterOperations(edges [][]int, values []int) int64 {
 		if len(g[x]) == 1 { // x 是叶子
 			return values[x]
 		}
-		loss := 0 // 不选 values[x]
+		var loss int // 不选 values[x]
 		for _, y := range g[x] {
 			if y != fa {
 				loss += dfs(y, x) // 计算以 y 为根的子树是健康时，失去的最小分数
