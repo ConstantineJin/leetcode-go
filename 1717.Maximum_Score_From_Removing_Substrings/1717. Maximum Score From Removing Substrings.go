73668d82758a83636cf25f3ac2140f2bdbd63af1
constantinejin <ConstantineJin@outlook.com>
2024-07-12 12:15:42 +0800

    Add 1717 solutions and tests

--- 1717.Maximum_Score_From_Removing_Substrings/1717. Maximum Score From Removing Substrings.go
+++ 1717.Maximum_Score_From_Removing_Substrings/1717. Maximum Score From Removing Substrings.go
@@ -4,18 +4,17 @@ func maximumGain(s string, x, y int) int {
 	var stackCnt0, stackCnt1 int // 不需要真的模拟一个栈，只需要统计字符 a 和 b 在栈中出现的次数
 	f := func(p, q rune, u, v int) (res int) {
 		for _, c := range s + "#" { // 在 s 结尾添加一个无关字符避免特判
-			if c == 'a' || c == 'b' {
-				if c == q { // 优先匹配分值较高的子串
-					if stackCnt0 > 0 {
-						stackCnt0--
-						res += u
-					} else {
-						stackCnt1++
-					}
+			switch c {
+			case q: // 优先匹配分值较高的子串
+				if stackCnt0 > 0 {
+					stackCnt0--
+					res += u
 				} else {
-					stackCnt0++
+					stackCnt1++
 				}
-			} else { // 其他字符可以被视作分隔符
+			case p:
+				stackCnt0++
+			default: // 其他字符可以被视作分隔符
 				res += v * min(stackCnt0, stackCnt1)
 				stackCnt0, stackCnt1 = 0, 0
 			}
