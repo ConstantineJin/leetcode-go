be9fcb8e64ffd9a597872bd3b7637000871bad71
constantinejin <ConstantineJin@outlook.com>
2024-08-18 18:33:16 +0800

    Add 0264 solutions and tests

---
+++ 0264.Ugly_Number_II/0264. Ugly Number II_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func Test_nthUglyNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Example1",
+			args: args{n: 10},
+			want: 12,
+		},
+		{
+			name: "Example2",
+			args: args{n: 1},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nthUglyNumber(tt.args.n); got != tt.want {
+				t.Errorf("nthUglyNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
