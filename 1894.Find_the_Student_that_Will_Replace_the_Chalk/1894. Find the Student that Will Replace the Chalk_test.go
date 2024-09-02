f8b7d4d11e4fc7b8fac270b9371f6566123a50ce
constantinejin <ConstantineJin@outlook.com>
2024-09-02 10:52:35 +0800

    Add 1894 solutions and tests

---
+++ 1894.Find_the_Student_that_Will_Replace_the_Chalk/1894. Find the Student that Will Replace the Chalk_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func Test_chalkReplacer(t *testing.T) {
+	type args struct {
+		chalk []int
+		k     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Example1",
+			args: args{
+				chalk: []int{5, 1, 5},
+				k:     22,
+			},
+			want: 0,
+		},
+		{
+			name: "Example2",
+			args: args{
+				chalk: []int{3, 4, 1, 2},
+				k:     25,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := chalkReplacer(tt.args.chalk, tt.args.k); got != tt.want {
+				t.Errorf("chalkReplacer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
