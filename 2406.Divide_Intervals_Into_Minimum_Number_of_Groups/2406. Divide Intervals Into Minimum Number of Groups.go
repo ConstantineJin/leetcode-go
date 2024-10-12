1e26dc3504ab1c77b23a21defc5a88399f0939d7
constantinejin <ConstantineJin@outlook.com>
2024-10-12 09:38:10 +0800

    Add 2406 solutions and tests

--- 2406.Divide_Intervals_Into_Minimum_Number_of_Groups/2406. Divide Intervals Into Minimum Number of Groups.go
+++ 2406.Divide_Intervals_Into_Minimum_Number_of_Groups/2406. Divide Intervals Into Minimum Number of Groups.go
@@ -1,8 +1,7 @@
 package main
 
-// 等价于求每个点最多被多少个区间覆盖
 func minGroups(intervals [][]int) (ans int) {
-	var diff [1e6 + 1]int
+	var diff [1e6 + 1]int // 等价于求每个点最多被多少个区间覆盖。可以使用差分数组维护
 	for _, interval := range intervals {
 		diff[interval[0]-1]++
 		diff[interval[1]]--
